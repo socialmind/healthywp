3caa0153c3fa50857e7b37882c22bdd4bd511d16
Apostolos Kritikos <akritiko@gmail.com>
2018-05-10 11:32:42 +0300

    documentation added

--- admin/partials/hwp-wordpress-plugin.php
+++ admin/partials/hwp-wordpress-plugin.php
@@ -71,117 +71,144 @@ class WordPressPlugin
         $this->numberOfIncompatiblePlugins = 0;
     }
 
+    /** @return string Returns the name */
     public function getName(){
         return $this->name;
     }
 
+    /** @param string $name */
     public function setName($name){
         $this->name = $name;
     }
 
+    /** @return string Returns the plugin's URI */
     public function getPluginURI(){
         return $this->pluginURI;
     }
 
+    /** @param string $pluginURI */
     public function setPluginURI($pluginURI){
         $this->pluginURI = $pluginURI;
     }
 
+    /** @return string Returns the plugin's version */
     public function getVersion(){
         return $this->version;
     }
 
+    /** @param string $version */
     public function setVersion($version){
         $this->version = $version;
     }
 
+    /** @return string Returns the plugin's description */
     public function getDescription(){
         return $this->description;
     }
 
+    /** @param string $description */
     public function setDescription($description){
         $this->description = $description;
     }
 
+    /** @return string Returns the plugin's author */
     public function getAuthor(){
         return $this->author;
     }
 
+    /** @param string $author */
     public function setAuthor($author){
         $this->author = $author;
     }
 
+    /** @return string Returns the plugin author's URI */
     public function getAuthorURI(){
         return $this->authorURI;
     }
 
+    /** @param string $authorURI */
     public function setAuthorURI($authorURI){
         $this->authorURI = $authorURI;
     }
 
+    /** @return string Returns the plugin's text domain */
     public function getTextDomain(){
         return $this->textDomain;
     }
 
+    /** @param string $textDomain */
     public function setTextDomain($textDomain){
         $this->textDomain = $textDomain;
     }
 
+    /** @return string Returns the plugin's domain path */
     public function getDomainPath(){
         return $this->domainPath;
     }
 
+    /** @param string $domainPath */
     public function setDomainPath($domainPath){
         $this->domainPath = $domainPath;
     }
 
+    /** @return string Returns the plugin's network */
     public function getNetwork(){
         return $this->network;
     }
 
+    /** @param string $network */
     public function setNetwork($network){
         $this->network = $network;
     }
 
+    /** @return string Returns the plugin's title */
     public function getTitle(){
         return $this->title;
     }
 
+    /** @param string $title */
     public function setTitle($title){
         $this->title = $title;
     }
 
+    /** @return string Returns the plugin's author name */
     public function getAuthorName(){
         return $this->authorName;
     }
 
+    /** @param string $authorName */
     public function setAuthorName($authorName){
         $this->authorName = $authorName;
     }
 
+    /** @return int Returns total reports of the plugin to healthywp database */
     public function getTotalReports(){
         return $this->totalReports;
     }
 
+    /** @param int $totalReports */
     public function setTotalReports($totalReports){
         $this->totalReports = $totalReports;
     }
-
+    
+    /** @return int Returns total reports of compatible plugins to healthywp database */
     public function getNumberOfCompatiblePlugins(){
         return $this->numberOfCompatiblePlugins;
     }
 
+    /** @param int $numberOfCompatiblePlugins */
     public function setNumberOfCompatiblePlugins($numberOfCompatiblePlugins){
         $this->numberOfCompatiblePlugins = $numberOfCompatiblePlugins;
     }
 
+    /** @return int Returns total reports of incompatible plugins to healthywp database */
     public function getNumberOfIncompatiblePlugins(){
         return $this->numberOfIncompatiblePlugins;
     }
 
+    /** @param int $numberOfInompatiblePlugins */
     public function setNumberOfIncompatiblePlugins($numberOfIncompatiblePlugins){
         $this->numberOfIncompatiblePlugins = $numberOfIncompatiblePlugins;
     }
-
 }
 ?>
